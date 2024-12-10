8dce76184774b014266508abb6325e89a1fc1f57
Jan Chruszcz <jrchruszcz@gmail.com>
2024-12-10 10:32:05 +0100

    Require devise mailer

--- api/config/application.rb
+++ api/config/application.rb
@@ -15,6 +15,8 @@ require "action_view/railtie"
 # require "rails/test_unit/railtie"
 
 require 'devise'
+require 'devise/mailer'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
